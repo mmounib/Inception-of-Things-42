3372644bd55182f308666f8fb627f891f234589a
Mouad Mounib <93907293+mmounib@users.noreply.github.com>
2024-12-12 14:55:16 +0100

    Update main-installation.sh

--- p3/scripts/main-installation.sh
+++ p3/scripts/main-installation.sh
@@ -1,6 +1,8 @@
-sudo k3d cluster create argocdIntegration
+sudo k3d cluster create argocdIntegration -p 8080:80@loadbalancer -p 8443:443@loadbalancer -p 8888:8888@loadbalancer
 sudo kubectl create namespace argocd
 sudo kubectl create namespace dev
+
+k3d kubeconfig get argocdIntegration > ~/.kube/config
 
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 sudo kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
@@ -10,4 +12,4 @@ sudo kubectl get pods -n argocd
 
 sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
 
-sudo kubectl apply -f ./configs/app.yaml -n argocd
+sudo kubectl apply -f ../configs/app.yaml -n argocd
